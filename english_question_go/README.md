2f11377a616594aae0a51a962db5ec67613e0d76
markforcode <markhuang801011@hotmail.com>
2017-08-07 15:52:54 +0800

    step4

---
+++ english_question_go/README.md
@@ -0,0 +1,19 @@
+
+$ meteor add-platform ios
+$ meteor add-platform android
+$ rm -rf server client
+$ meteor remove blaze-html-templates
+$ meteor add angular-templates
+$ meteor add dab0mb:ionic-assets
+$ meteor npm install angular@^1.5.8 --save
+$ meteor npm install angular-animate@^1.5.8 --save
+$ meteor npm install angular-sanitize@^1.5.8 --save
+$ meteor npm install angular-ui-router@^0.3.1 --save
+$ meteor npm install ionic-scripts --save
+$ meteor npm install babel-runtime --save
+$ meteor add mobile-status-bar
+$ meteor add launch-screen
+$ meteor npm install angular-meteor --save
+$ meteor npm install angular-ecmascript --save
+$ meteor add fourseven:scss
+$ meteor npm install angular-moment --save
--- english_question_go/client/scripts/routes.js
+++ english_question_go/client/scripts/routes.js
@@ -1,6 +1,7 @@
 import { Config } from 'angular-ecmascript/module-helpers';
  
 import chatsTemplateUrl from '../templates/chats.html';
+import chatTemplateUrl from '../templates/chat.html';
 import tabsTemplateUrl from '../templates/tabs.html';
  
 export default class RoutesConfig extends Config {
@@ -19,7 +20,19 @@ export default class RoutesConfig extends Config {
             controller: 'ChatsCtrl as chats'
           }
         }
-      });
+      })
+      .state('tab.chat', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: chatTemplateUrl,
+            controller: 'ChatCtrl as chat'
+          }
+
+        }
+
+      })
+      ;
  
     this.$urlRouterProvider.otherwise('tab/chats');
   }
